536323b7cf68356dc2959223b92c3e74c4d49b0e
r1verfuture <87302454+r1verfuture@users.noreply.github.com>
2022-12-20 16:39:51 +0900

    Create 배열의_유사도_1.swift

---
+++ Programmers/배열의_유사도_1.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ s1:[String], _ s2:[String]) -> Int {
+  var result: Int = 0
+  s1.forEach {
+    if s2.contains($0) {
+      result += 1
+    }
+  }
+  return result
+}
